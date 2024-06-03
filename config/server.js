d6a0693fa54474ed9f459b6142ed3186c4c1901f
王伟 <wangwei@wangweideMacBook-Pro.local>
2024-06-03 16:31:28 +0800

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '832382fd91af54fc2df74491cd9ccea1'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '577b544e-0b3c-4bf6-a93b-7cb4a3d236cb'
+};
